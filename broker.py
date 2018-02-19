3f74c2be7eb70b9fb575a75a34e96c1145ec0148
rounaksalim95 <rounaksalim@gmail.com>
2018-02-18 18:33:25 -0600

    Update middleware and broker

--- broker.py
+++ broker.py
@@ -1,1 +1,39 @@
+# Requires sortedcontainers (pip install sortedcontainers)
+
 import middleware
+from sortedcontainers import SortedListWithKey
+
+
+# Dictionary that topics to sorted lists that keep track of the avaialable publishers (sorted on ownership strength)
+topics_dict = {}
+
+test_address = "tcp://*:7777"
+# Start listening for publishers and subscribers 
+socket = middleware.start_listening(test_address)
+
+
+# Some helper functions 
+'''
+Function that adds the provided publisher to topics_dict
+publisher_info: Information on the publisher 
+'''
+def add_publisher(publisher_info):
+    print(publisher_info)
+    topic = publisher_info[2]
+    publisher = (publisher_info[1], int(publisher_info[3]), int(publisher_info[4]))
+    if topic in topics_dict:
+        topics_dict[topic].add(publisher)
+    else: 
+        topics_dict[topic] = SortedListWithKey(key=lambda x: -x[1])
+        topics_dict[topic].add(publisher)
+
+
+# Listen to incoming publisher and subscriber requests 
+while True: 
+    message = socket.recv().decode()    # decode uses utf-8 encoding by default 
+    parts = message.split(',')
+    # If publisher makes request then add them to topics_dict appropriately 
+    if (parts[0] == 'pub'): 
+        add_publisher(parts)
+        socket.send(b"Added publisher")
+        print(topics_dict)
--- middleware.py
+++ middleware.py
@@ -10,7 +10,7 @@ import zmq
 Function that can be called to register the publisher with the broker 
 Returns the response received by the broker 
 address: Address that the publisher is pushing content from 
-broker_address: Address of the broker that the request needs to be sent to (including protocol)
+broker_address: Address of the broker that the request needs to be sent to (include protocol)
 topic: Topic that the publisher is pushing content for
 ownership_strength: The ownership strength of the publisher (default value is 0)
 history: The amount of history that the publisher maintains (default value is 0)
@@ -41,7 +41,7 @@ def publish(topic, content):
 '''
 Function that can be called to register the subscriber with the broker 
 Returns the address of the best publisher available 
-broker_address: Address of the broker that the request needs to be sent to (including protocol)
+broker_address: Address of the broker that the request needs to be sent to (include protocol)
 topic: Topic that the subscriber wants to subscribe to 
 history: The amount of history that the subscriber wants the publisher to maintain (default value is 0)
 Returns publisher that the subscriber should subscribe to 
@@ -61,7 +61,7 @@ def register_sub(broker_address, topic, history = 0):
 Function that the subscriber can use to inform the broker that it has lost the publisher it was connected to;
 broker responds by providing next best publisher 
 Returns the address of the next best publisher available 
-broker_address: Address of the broker that the request needs to be sent to (including protocol)
+broker_address: Address of the broker that the request needs to be sent to (include protocol)
 publisher: The publisher that the subscriber was subscribed to (this can be used by the broker to remove the inactive publisher)
 topic: Topic that the subscriber wants to subscribe to 
 history: The amount of history that the subscriber wants the publisher to maintain (default value is 0)
@@ -76,3 +76,25 @@ def notify(broker_address, publisher, topic, history = 0):
     response = socket.recv()
     context.destroy()
     return response
+
+
+# Wrapper functions that are useful for the broker 
+'''
+Funtion that binds a socket for the broker to receive messages from the publishers and subscribers 
+Returns the bound socket 
+address: Address to bind the socket to (include protocol)
+'''
+def start_listening(address):
+    context = zmq.Context()
+    print('Binding socket to ', address) 
+    socket = context.socket(zmq.REP)
+    socket.bind(address) 
+    return socket 
+
+'''
+Funtion that destroys the provided socket  
+socket: Socket to destroy 
+'''
+def stop_listening(socket):
+    socket.destroy()
+    print("Socket destroyed") 
